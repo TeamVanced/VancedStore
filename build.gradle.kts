b8292b62612ca48fafc0921b2aaf4d4f63d0cf30
X1nto <burtkni@yandex.ru>
2022-05-04 19:27:56 +0400

    update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.1.2" apply false
+    id("com.android.application") version "7.1.3" apply false
     kotlin("android") version "1.6.20" apply false
     kotlin("plugin.serialization") version "1.6.20" apply false
     id("com.google.devtools.ksp") version "1.6.10-1.0.4" apply false
